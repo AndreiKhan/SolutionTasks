13bf83cdb8bc194c1370720bb6c933072d8d1d33
«Andrei» <Spectacul9r@mail.ru>
2022-07-10 16:26:19 +0500

    add Solution for home work task 2

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Вводим два числа
+string? inputLineOne = Console.ReadLine();
+string? inputLineTwo = Console.ReadLine();
+
+//Проверяем ввели мы числа
+if (inputLineOne != null && inputLineTwo != null)
+{
+    //Превраращаем их в целочисленные
+    int outputLineOne = int.Parse(inputLineOne);
+    int outputLineTwo = int.Parse(inputLineTwo);
+
+    //Ищем боольше число из двух
+    if (outputLineOne > outputLineTwo)
+    {
+        Console.WriteLine("Max = " + outputLineOne);
+    }
+    else
+    {
+        Console.WriteLine("Max = " + outputLineTwo);
+    }
+}

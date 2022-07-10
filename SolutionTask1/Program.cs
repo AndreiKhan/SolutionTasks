d13ab49425b794055680cefdd5a1d86a8505abff
«Andrei» <Spectacul9r@mail.ru>
2022-07-10 15:01:09 +0500

    add Solution for task 0

---
+++ SolutionTask1/Program.cs
@@ -0,0 +1,26 @@
+﻿string? inputLineOne = Console.ReadLine();
+string? inputLineTwo = Console.ReadLine();
+
+if(inputLineOne != null && inputLineTwo != null);
+{
+    int NumberOne = int.Parse(inputLineOne);
+    int NumberTwo = int.Parse(inputLineTwo);
+
+    //if(NumberOne == NumberTwo * NumberTwo)
+    //{
+    //    Console.WriteLine("Yes");
+    //}
+    //else
+    //{
+    //    Console.WriteLine("No");
+    //}
+
+    if(NumberOne == Math.Sqrt(NumberTwo))
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}

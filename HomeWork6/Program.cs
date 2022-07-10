866bdfa55171c13b41c0e4cc6beb400e5062af02
«Andrei» <Spectacul9r@mail.ru>
2022-07-10 17:50:07 +0500

    add Solution for home work task 6

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,31 @@
+﻿//------------------Задача 6--------------------
+//-----------Определение четного числа----------
+//----------------------------------------------
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    //int inputNumber = int.Parse(inputLine);
+    //
+    // if(inputNumber % 2 == 0)
+    // {
+    //     Console.WriteLine(inputNumber + " - Четное число");                  //Первый вариант через if
+    // }
+    // else
+    // {
+    //     Console.WriteLine(inputNumber + " - Нечетное число");
+    // }
+
+    
+    //Раскладываем число в массиве
+    char[] M = inputLine.ToCharArray();
+    //Берем число под последним индексом и проверяем четное ли оно
+    if((M[M.Length - 1] % 2) == 0)
+    {
+        Console.WriteLine(inputLine + " - Четное число");                       //Второй вариант через массив и if
+    }
+    else
+    {
+        Console.WriteLine(inputLine + " - Нечетное число");
+    }
+}

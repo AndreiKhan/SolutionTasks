3a3d843ab513aae4341c38af02a8104a1bc0bac8
«Andrei» <Spectacul9r@mail.ru>
2022-07-10 15:44:07 +0500

    add Solution for task 3

---
+++ SolutionTask3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Показывает числа от -N до N
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+
+    int startNumber  = 1;
+
+    string lineOutput = "0";
+
+    while (startNumber <= inputNumber)
+    {
+        lineOutput = startNumber * (-1) + ", " + lineOutput + ", " + startNumber;
+        startNumber++;
+    }
+
+    Console.WriteLine(lineOutput);
+}
+
+// if (inputLine != null)
+// {
+//     int inputNumber = int.Parse(inputLine);
+
+//     int startNumber  = (inputNumber * -1);
+
+//     string lineOutput = "";                                  !Это вариант где шагов будет больше!
+
+//     while (startNumber < inputNumber)
+//     {
+//         lineOutput = lineOutput + startNumber + ", ";
+//         startNumber++;
+//     }
+
+//     lineOutput = lineOutput + inputNumber;
+
+//     Console.WriteLine(lineOutput);
+// }

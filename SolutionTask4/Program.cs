3e329fd8859a8fc2e6c1c09708b7ee6930bb0a92
«Andrei» <Spectacul9r@mail.ru>
2022-07-10 16:01:21 +0500

    add Solution for task 4

---
+++ SolutionTask4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Находим последнее число из трехзначного
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    //int inputNumber = int.Parse(inputLine);               //Первый вариант через нахождение остатка
+    //int outputNumber = inputNumber % 10;
+    //Console.WriteLine(outputNumber);
+
+    //string outputNumber = inputLine.Substring(2, 1);      //Второй вариант одной строкой
+    //Console.WriteLine(outputNumber);
+    
+    char[] M = inputLine.ToCharArray();                     //Третий вариант через массив
+    Console.WriteLine(M[2]);
+}

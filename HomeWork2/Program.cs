0052ba52ac46fcf3d99298cd343b143efe58af02
«Andrei» <Spectacul9r@mail.ru>
2022-07-10 17:09:32 +0500

    add Solution for home work task 4 and new solution for task 2

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -1,21 +1,31 @@
-﻿//Вводим два числа
+﻿//------------------Задача 2--------------------
+//-------Определение больше числа из двух-------
+//----------------------------------------------
+//Вводим два числа
 string? inputLineOne = Console.ReadLine();
 string? inputLineTwo = Console.ReadLine();
 
-//Проверяем ввели мы числа
+//Проверяем ввели ли мы числа
 if (inputLineOne != null && inputLineTwo != null)
 {
     //Превраращаем их в целочисленные
     int outputLineOne = int.Parse(inputLineOne);
     int outputLineTwo = int.Parse(inputLineTwo);
 
-    //Ищем боольше число из двух
-    if (outputLineOne > outputLineTwo)
-    {
-        Console.WriteLine("Max = " + outputLineOne);
-    }
-    else
-    {
-        Console.WriteLine("Max = " + outputLineTwo);
-    }
+    // //Ищем большее число из двух
+    // if (outputLineOne > outputLineTwo)
+    // {
+    //     Console.WriteLine("Max = " + outputLineOne);
+    // }
+    // else                                                                         //Первый вариант решения через if
+    // {
+    //     Console.WriteLine("Max = " + outputLineTwo);
+    // }
+
+
+    // int maxNumber = Math.Max(outputLineOne, outputLineTwo);
+    // Console.WriteLine("Max = " + Math.Max(outputLineOne, outputLineTwo));        //Второй вариант решения через Math.Max
+
+
+    Console.WriteLine("Max = " + Math.Max(outputLineOne, outputLineTwo));           //Третий вариант черз Math.Max без создания переменной
 }
---
+++ HomeWork4/Program.cs
@@ -0,0 +1,47 @@
+﻿//Вводим три числа
+string? inputLineOne = Console.ReadLine();
+string? inputLineTwo = Console.ReadLine();
+string? inputLineThree = Console.ReadLine();
+
+//Проверяем ввели ли мы числа
+if (inputLineOne != null && inputLineTwo != null)
+{
+    //Превраращаем их в целочисленные
+    int outputLineOne = int.Parse(inputLineOne);
+    int outputLineTwo = int.Parse(inputLineTwo);
+    int outputLineThree = int.Parse(inputLineThree);
+
+    //Ищем большее число из трех
+    //Определяем, первое число больше второго?
+    // if (outputLineOne > outputLineTwo)
+    // {
+    //     //Определяем, первое число больше третьего?
+    //     if(outputLineOne > outputLineThree)
+    //     {
+    //         Console.WriteLine("Max = " + outputLineOne);
+    //     }
+    //     else
+    //     {
+    //         Console.WriteLine("Max = " + outputLineThree);                                           //Первый вариант через if
+    //     }
+    // }
+    // else
+    // {
+    //     //Определяем, второе число больше третьего?
+    //     if(outputLineTwo > outputLineThree)
+    //     {
+    //         Console.WriteLine("Max = " + outputLineTwo);
+    //     }
+    //     else
+    //     {
+    //         Console.WriteLine("Max = " + outputLineThree);
+    //     }
+    // }
+
+
+    // int maxNumber = Math.Max(outputLineOne, Math.Max(outputLineTwo, outputLineThree));               //Второй вариант через Мath.Max
+    // Console.WriteLine("Max = " + maxNumber);
+
+
+    Console.WriteLine("Max = " + Math.Max(outputLineOne, Math.Max(outputLineTwo, outputLineThree)));    //Третий вариант черз Math.Max без создания переменной
+}

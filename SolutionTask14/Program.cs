f174cac6d828387ac60ed1f4d6078e4ccadd2901
«Andrei» <Spectacul9r@mail.ru>
2022-07-13 12:50:49 +0500

    add solution for task 14

---
+++ SolutionTask14/Program.cs
@@ -0,0 +1,23 @@
+﻿//вариант 1
+string reminderSearcher(string str)
+{
+    if (str != null)
+    {
+        string answer = "";
+        int num = int.Parse(str);
+        if ((num % 7 == 0) && (num % 23 == 0))
+        {
+            answer = "кратно 7 и 23";
+        }
+        else
+        {
+            answer = "не кратно 7 и 23";
+        }
+        return answer;
+    }
+        return "";
+}
+
+string? inputOne = Console.ReadLine();
+
+Console.WriteLine(reminderSearcher(inputOne));

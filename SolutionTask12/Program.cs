85a3dce6954a9a55567bfebd8a0160570cc5ed4c
«Andrei» <Spectacul9r@mail.ru>
2022-07-13 12:35:15 +0500

    add solution for task 12

---
+++ SolutionTask12/Program.cs
@@ -0,0 +1,36 @@
+﻿// //вариант 1
+// string? inp1 = Console.ReadLine();
+// string? inp2 = Console.ReadLine();
+
+// if (inp1 != null && inp2 != null)
+// {
+//     int inpNum1  = int.Parse(inp1);
+//     int inpNum2  = int.Parse(inp2);
+
+//     int div = inpNum2 % inpNum1;
+
+//     if (div == 0)
+//     {
+//         Console.WriteLine("yes");
+//     }
+//     else
+//     {
+//         Console.WriteLine(div);
+//     }
+// }
+
+// //вариант 2
+// string? inp1 = Console.ReadLine();
+// string? inp2 = Console.ReadLine();
+
+// int inpNum1  = int.Parse(inp1);
+// int inpNum2  = int.Parse(inp2);
+
+// if (inpNum2 % inpNum1 == 0)
+// {
+//     Console.WriteLine("yes");
+// }
+// else
+// {
+//     Console.WriteLine(inpNum2 % inpNum1);
+// }

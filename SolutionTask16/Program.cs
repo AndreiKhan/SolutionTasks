dc20edd0307d3a1bbe80369499027033d7e30664
«Andrei» <Spectacul9r@mail.ru>
2022-07-13 13:07:38 +0500

    add solution for task 16

---
+++ SolutionTask16/Program.cs
@@ -0,0 +1,21 @@
+﻿//вариант 1
+string? inputOne = Console.ReadLine();
+string? inputTwo = Console.ReadLine();
+
+if ((inputOne != null) && (inputTwo != null))
+{
+    int inputNumberFirst = int.Parse(inputOne);
+    int inputNumberSecond = int.Parse(inputTwo);
+
+    int a = inputNumberFirst * inputNumberFirst;
+    int b = inputNumberSecond * inputNumberSecond;
+
+    if ((a == inputNumberSecond) || (b == inputNumberFirst))
+    {
+        Console.WriteLine("yes");
+    }
+    else
+    {
+        Console.WriteLine("no");
+    }
+}

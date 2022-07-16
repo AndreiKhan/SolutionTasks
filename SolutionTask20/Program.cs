90e984124f0327923c4b2302777211c43a8facce
«Andrei» <Spectacul9r@mail.ru>
2022-07-16 14:59:25 +0500

    add solution for task 20

---
+++ SolutionTask20/Program.cs
@@ -0,0 +1,32 @@
+﻿int coordXpointA;
+int coordYpointA;
+int coordXpointB;
+int coordYpointB;
+double lengthAB;
+
+//считывает координаты точек А и В
+void readDataOfPoint()
+{
+    Console.WriteLine("Введите координату Х точки А");
+    coordXpointA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y точки А");
+    coordYpointA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Х точки B");
+    coordXpointB = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y точки B");
+    coordYpointB = int.Parse(Console.ReadLine());
+}
+
+//вычисляет расстояние между точками А и В
+void calculateLength()
+{
+    lengthAB = Math.Sqrt(Math.Pow((coordXpointA - coordXpointB), 2) + Math.Pow((coordYpointA - coordYpointB), 2));
+}
+
+readDataOfPoint();
+calculateLength();
+
+Console.WriteLine(lengthAB);

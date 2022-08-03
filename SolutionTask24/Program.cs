e5113e76cfab2010f53e1d981907db08ab4d4ad8
«Andrei» <Spectacul9r@mail.ru>
2022-08-03 22:02:28 +0500

    add solution for task 24

---
+++ SolutionTask24/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+
+int inputnumber = int.Parse(Console.ReadLine());
+int t;
+
+void VariantSimple()
+{
+    int sumOfNumbers = 0;
+
+    for(int i = 1; i <= inputnumber; i++)
+    {
+        sumOfNumbers += i;
+    }
+
+    Console.WriteLine("Сумма чисел от 1 до " + inputnumber + " = " + sumOfNumbers);
+}
+
+void VariantGauss()
+{
+    int sumOfNumbers = 0;
+
+    sumOfNumbers = (inputnumber * (inputnumber + 1))/2;
+
+    Console.WriteLine("Сумма чисел от 1 до " + inputnumber + " = " + sumOfNumbers);
+}
+
+t = Environment.TickCount;
+
+VariantSimple();
+Console.WriteLine("Simple time: {0} ms", Environment.TickCount - t);
+t = Environment.TickCount;
+VariantGauss();
+Console.WriteLine("Simple time: {0} ms", Environment.TickCount - t);

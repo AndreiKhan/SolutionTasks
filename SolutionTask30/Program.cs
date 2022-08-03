f4261ab345b83d5a5a7ce8ed6db429196a70f98b
«Andrei» <Spectacul9r@mail.ru>
2022-08-03 23:20:42 +0500

    add solution for task 30

---
+++ SolutionTask30/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Random numberSintezator = new Random();
+
+void VariantNaive()
+{
+    int i = 0;
+
+    Console.Write("[");
+    while(i < 7)
+    {
+        Console.Write(numberSintezator.Next(0,2) + ",");
+        i++;
+    }
+    Console.Write(numberSintezator.Next(0,2));
+    Console.Write("]");
+}
+
+VariantNaive();
